362459e5917b377dee11ccf0b1792039509ed3e3
mohammad hadi arvin <mammad.arvin@gmail.com>
2023-01-16 12:42:53 +0330

    project done

--- src/index.js
+++ src/index.js
@@ -1,17 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+// style
+import "./index.css";
+
+//components
+import App from "./App";
+
+//pakages
+import axios from "axios";
+import { BrowserRouter } from "react-router-dom";
+
+//axios config
+axios.interceptors.response.use((response) => {
+    if (
+        response.request.responseURL ===
+        "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=true&price_change_percentage=24h"
+    ) {
+        return response.data;
+    }
+    return response;
+});
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
